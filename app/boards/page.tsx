baad1471caeddaec0a607abe202fa95b4fbbd1b2
inioluwabello <inioluwabellodavid@gmail.com>
2023-08-06 08:31:33 +0100

    initial deployment 1

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 /* Components */
 import { Board } from "../components/Board/Board";
 import React, { useEffect } from "react";
@@ -9,26 +7,8 @@ import { useAuthContext } from "@/lib/context/AuthContext";
 import { useRouter } from "next/navigation";
 
 export default function IndexPage() {
-    const { user } = useAuthContext()
-    const router = useRouter()
-
-    React.useEffect(() => {
-        if (user == null) router.push("/")
-    }, [user, router])
-
-    const theme = useSelector(selectTheme)
-
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        // On page load, check if there's a saved theme in localStorage
-        const savedTheme = localStorage.getItem('savedTheme');
-        if (savedTheme) {
-            dispatch(pageSlice.actions.setTheme(savedTheme));
-        }
-    }, [dispatch]);
-
-    return <Board theme={theme} />
+    
+    return <Board />
 }
 
 export const metadata = {
--- app/components/Board/Board.tsx
+++ app/components/Board/Board.tsx
@@ -4,17 +4,37 @@ import { useEffect } from "react";
 import { Loading } from "../LoadingBar/Loading"
 import { LeftPane } from "./LeftPane/LeftPane"
 import { RightPane } from "./RightPane/RightPane";
-import { getBoardsAsync, getSelectedBoard, selectBoards, useDispatch, useSelector } from "@/lib/redux";
+import { getBoardsAsync, getSelectedBoard, pageSlice, selectBoards, selectTheme, useDispatch, useSelector } from "@/lib/redux";
+import { useAuthContext } from "@/lib/context/AuthContext";
+import { useRouter } from "next/navigation";
 
-export const Board = ({ theme }: { theme: string }) => {
+export const Board = () => {
 
     const boards = useSelector(selectBoards);
     const selectedBoard = useSelector(getSelectedBoard)
+    const theme = useSelector(selectTheme)
+
 
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(getBoardsAsync())
     }, [])
+
+    const { user } = useAuthContext()
+    const router = useRouter()
+
+    useEffect(() => {
+        if (user == null) router.push("/")
+    }, [user, router])
+
+
+    useEffect(() => {
+        // On page load, check if there's a saved theme in localStorage
+        const savedTheme = localStorage.getItem('savedTheme');
+        if (savedTheme) {
+            dispatch(pageSlice.actions.setTheme(savedTheme));
+        }
+    }, [dispatch]);
 
     return (
         <main className={`board ${theme}`}>
